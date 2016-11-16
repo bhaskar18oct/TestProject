b7ab0821dfea6e819fba21c1dc84864e6720da47
bhaskar <bhaskar.loganathan@gmail.com>
2016-11-16 10:49:36 +0530

    java file

---
+++ KeywordStatic.java
@@ -0,0 +1,14 @@
+
+
+public class KeywordStatic {
+	
+	static int e;
+	static String e1;
+	
+	
+	public static void main(String[] args){
+		System.out.println("default value of integer : " +e);
+		System.out.print("default value of string : " +e1);
+	}
+
+}
